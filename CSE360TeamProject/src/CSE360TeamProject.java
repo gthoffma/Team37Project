cc2ca23798e19e6dc41d0bcad9c607445a4e022e
ghoff <ghoff@Lenovo-PC.3520.dhcp.asu.edu>
2019-11-22 10:02:00 -0700

    just some testing of Javafx; makes a window and a button in the window. from GUI part1

--- CSE360TeamProject/src/CSE360TeamProject.java
+++ CSE360TeamProject/src/CSE360TeamProject.java
@@ -1,9 +1,18 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
 
-
-public class CSE360TeamProject {
-
+public class CSE360TeamProject extends Application{
+	public static void main(String[] args) {
+        launch(args);
+    }
+	public void start(Stage primaryStage) {
+		Button OKButton = new Button("OK");
+		Scene scene = new Scene(OKButton, 200, 300);
+		primaryStage.setTitle("CSETeamProject360 Testing");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
