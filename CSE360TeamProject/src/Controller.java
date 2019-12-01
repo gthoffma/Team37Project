2106a8192c76a65c2c03693efbab11db1a99adf5
jcoro <john@coronite.net>
2019-12-01 13:41:59 -0700

    Add file upload capability.

--- CSE360TeamProject/src/Controller.java
+++ CSE360TeamProject/src/Controller.java
@@ -6,6 +6,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
     public Text text1;
@@ -37,15 +45,17 @@ public class Controller {
     public Text high;
     public Text low;
     public BarChart<Number, String> barChart;
+    public javafx.scene.layout.GridPane GridPane;
+    public ArrayList<Float> grades = new ArrayList<>();
 
     private XYChart.Series<Number, String> series1 = new XYChart.Series<>();
 
     @FXML
-    public void initialize(){
+    public void initialize() {
         loadData();
     }
 
-    private void loadData(){
+    private void loadData() {
         series1.getData().add(new XYChart.Data<>(20.0f, "0-10"));
         series1.getData().add(new XYChart.Data<>(30.0f, "11-20"));
         series1.getData().add(new XYChart.Data<>(40.0f, "21-30"));
@@ -59,6 +69,80 @@ public class Controller {
         barChart.getData().add(series1);
 
     }
+
+    public void onLoadDataClicked() {
+        FileChooser fileChooser = new FileChooser();
+        Stage stage = (Stage) GridPane.getScene().getWindow();
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            String extension;
+            String fileName = file.getName();
+            int i = fileName.lastIndexOf('.');
+            if (i > 0) {
+                extension = fileName.substring(i + 1);
+                if (extension.equals("csv")) {
+                    readCSVFile(file);
+                } else if (extension.equals("txt")) {
+                    readTXTFile(file);
+                } else {
+                    display.setText("file extension: " + extension + "not recognized");
+                    display.setStyle("-fx-text-fill: red ;");
+                }
+
+            }
+
+        }
+    }
+
+    private void readCSVFile(File file) {
+        //System.out.println(file);
+        String line;
+        String cvsSplitBy = ",";
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // remove zero-width space
+                line = line.replace("\uFEFF", "");
+                System.out.println(line + "\n");
+                String[] gradeLineStringArray = line.split(cvsSplitBy);
+                for (String s : gradeLineStringArray) {
+                    Float gradeFloat = Float.parseFloat(s);
+                    grades.add(gradeFloat);
+                }
+
+                printGrades(grades);
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readTXTFile(File file) {
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+            while ((line = br.readLine()) != null) {
+                Float gradeFloat = Float.parseFloat(line.trim());
+                grades.add(gradeFloat);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        printGrades(grades);
+
+    }
+
+    private void printGrades(ArrayList<Float> grades) {
+        for (Float g : grades) {
+            System.out.println(g + " ");
+        }
+    }
 }
+
 
 
