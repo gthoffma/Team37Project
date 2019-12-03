6eec42f43b7ce4ba8f611b848741edcfa9f96259
ghoff <ghoff@Lenovo-PC>
2019-12-03 12:35:22 -0700

    test commit

--- CSE360TeamProject/src/Controller.java
+++ CSE360TeamProject/src/Controller.java
@@ -46,8 +46,8 @@ public class Controller {
     public BarChart<Number, String> barChart;
     public javafx.scene.layout.GridPane GridPane;
     public ArrayList<Float> grades = new ArrayList<>();
-    public Float highBound = 100.0f;
-    public Float lowBound = 0.0f;
+    public float highBound = 100.0f;
+    public float lowBound = 0.0f;
     public int numberOfEntries = 0;
     public float meanValue;
     public float medianValue;
@@ -55,102 +55,106 @@ public class Controller {
     public float highValue;
     public float lowValue;
     public XYChart.Series<Number, String> series1 = new XYChart.Series<>();
-    public String reportFile = "report.txt";
+    public String reportFile = "report.txt"; // TODO: if this is just a file name, it should be a constant -JC
+    public String logString;
 
     @FXML
     public void initialize() {
         updateNumberOfEntries(grades);
         display.setText("Welcome to Grade Analytics!\nLoad a file or enter grades manually to begin. ");
     }
-    
+
     /* TODO the report can be written to a file for each method called
-     *  for instance, when the clear data method is called, we can write to the 
+     *  for instance, when the clear data method is called, we can write to the
      *  file named report.txt
+     * I think a better way is to append to a String instance variable and to write the contents of it to the file only when the
+     * Create Report button is clicked. - JC
      */
 
+    //TODO the grades may not be from 0 to 100,
+    // we should divide the graph into equal parts from the high value
+    // to the low value
+    // i.e., if the user set the boundaries from -10 to 120, we could have 10 divisions of 
+    // size 13
+    // I put together a fix for this, but it needs testing. -JC
 
-    private void populateHistogram(ArrayList<Float> grades) {
-    	//barChart.getData().removeAll();
+    /**
+     * Populates the grade histogram.
+     *
+     * @param grades    - The arrayList of grades
+     * @param highBound - The current highBound
+     * @param lowBound  - The current lowBound
+     */
+    private void populateHistogram(ArrayList<Float> grades, float highBound, float lowBound) {
         CategoryAxis yAxis = (CategoryAxis) barChart.getYAxis();
         NumberAxis xAxis = (NumberAxis) barChart.getXAxis();
         xAxis.setLabel("Frequency");
         yAxis.setLabel("Grade Distribution");
-        
-        //gradeDistribution holds an int array representing the 
-        // number of inputs in that range
-        int[] gradeDistribution = new int[10];
+        float divisionSize = (highBound - lowBound) / 10.0f;
 
-        //initialize the number of grades in each division to 0
-        for(int i = 0; i < gradeDistribution.length; i++) {
-        	gradeDistribution[i] = 0;
-        }
-
-        // string array for distribution labels
-        String[] distLabelArr = new String[10];
+        int firstDivision = 0;
+        int secondDivision = 0;
+        int thirdDivision = 0;
+        int fourthDivision = 0;
+        int fifthDivision = 0;
+        int sixthDivision = 0;
+        int seventhDivision = 0;
+        int eighthDivision = 0;
+        int ninthDivision = 0;
+        int tenthDivision = 0;
         for (Float g : grades) {
-        	//for each division of 10, if the element g in grades is between the 
-        	// values in the division, increase the gradeDistribution at that index
-        	// NOTE: integer division is done to prevent the graph from having long
-        	// decimal places
-            if (g >= lowBound && g < (highBound-lowBound)/10 + lowBound) {
-                gradeDistribution[0]++;
-            } else if (g >= (highBound-lowBound)/10 + lowBound &&  g < 2*(highBound-lowBound)/10 + lowBound) {
-                gradeDistribution[1]++;
-            } else if (g >= 2*(highBound-lowBound)/10 + lowBound &&  g < 3*(highBound-lowBound)/10 + lowBound) {
-                gradeDistribution[2]++;
-            } else if (g >= 3*(highBound-lowBound)/10 + lowBound &&  g < 4*(highBound-lowBound)/10 + lowBound) {
-                gradeDistribution[3]++;
-            } else if (g >= 4*(highBound-lowBound)/10 + lowBound &&  g < 5*(highBound-lowBound)/10 + lowBound) {
-                gradeDistribution[4]++;;
-            } else if (g >= 5*(highBound-lowBound)/10 + lowBound &&  g < 6*(highBound-lowBound)/10 + lowBound) {
-                gradeDistribution[5]++;
-            } else if (g >= 6*(highBound-lowBound)/10 + lowBound &&  g < 7*(highBound-lowBound)/10 + lowBound) {
-                gradeDistribution[6]++;
-            } else if (g >= 7*(highBound-lowBound)/10 + lowBound &&  g < 8*(highBound-lowBound)/10 + lowBound) {
-                gradeDistribution[7]++;
-            } else if (g >= 8*(highBound-lowBound)/10 + lowBound &&  g < 9*(highBound-lowBound)/10 + lowBound) {
-                gradeDistribution[8]++;
-            } else if (g >= 9*(highBound-lowBound)/10 + lowBound &&  g < highBound) {
-                gradeDistribution[9]++;
+            if (g >= lowBound && g < lowBound + (1 * divisionSize)) {
+                firstDivision++;
+            } else if (g >= lowBound + (1 * divisionSize) && g < lowBound + (2 * divisionSize)) {
+                secondDivision++;
+            } else if (g >= lowBound + (2 * divisionSize) && g < lowBound + (3 * divisionSize)) {
+                thirdDivision++;
+            } else if (g >= lowBound + (3 * divisionSize) && g < lowBound + (4 * divisionSize)) {
+                fourthDivision++;
+            } else if (g >= lowBound + (4 * divisionSize) && g < lowBound + (5 * divisionSize)) {
+                fifthDivision++;
+            } else if (g >= lowBound + (5 * divisionSize) && g < lowBound + (6 * divisionSize)) {
+                sixthDivision++;
+            } else if (g >= lowBound + (6 * divisionSize) && g < lowBound + (7 * divisionSize)) {
+                seventhDivision++;
+            } else if (g >= lowBound + (7 * divisionSize) && g < lowBound + (8 * divisionSize)) {
+                eighthDivision++;
+            } else if (g >= lowBound + (8 * divisionSize) && g < lowBound + (9 * divisionSize)) {
+                ninthDivision++;
+            } else if (g >= lowBound + (9 * divisionSize) && g <= highBound) {
+                tenthDivision++;
             }
 
-            //set the upper and lower distribution labels separately, then
-            // iterate through the other labels
-            distLabelArr[0] = lowBound.toString()+"-"+Float.toString((highBound-lowBound)/10 + lowBound);
-            distLabelArr[9] = Float.toString(9*(highBound-lowBound)/10 + lowBound)+"-"+highBound.toString();
-            for(int i = 1; i < 9; i++) {
-            	distLabelArr[i] = Float.toString(i*(highBound-lowBound)/10 + lowBound)+"-"+Float.toString((i+1)*(highBound-lowBound)/10 + lowBound);
-            }
         }
-        //fill the graph with the labels and data 
-        for(int i = 0; i < 10; i++) {
-        	series1.getData().add(new XYChart.Data<>(gradeDistribution[i], distLabelArr[i]));
-        }
-        
-        yAxis.setCategories(FXCollections.observableArrayList(distLabelArr[0],distLabelArr[1],distLabelArr[2],distLabelArr[3],
-        		distLabelArr[4],distLabelArr[5],distLabelArr[6],distLabelArr[7],distLabelArr[8],distLabelArr[9]));
-        
-        //old way of adding data
-        /*
-        series1.getData().add(new XYChart.Data<>(zeroToTen, "0-10"));
-        series1.getData().add(new XYChart.Data<>(elevenToTwenty, "11-20"));
-        series1.getData().add(new XYChart.Data<>(twentyOneToThirty, "21-30"));
-        series1.getData().add(new XYChart.Data<>(thirtyOneToForty, "31-40"));
-        series1.getData().add(new XYChart.Data<>(fortyOneToFifty, "41-50"));
-        series1.getData().add(new XYChart.Data<>(fiftyOneToSixty, "51-60"));
-        series1.getData().add(new XYChart.Data<>(sixtyOneToSeventy, "61-70"));
-        series1.getData().add(new XYChart.Data<>(seventyOneToEighty, "71-80"));
-        series1.getData().add(new XYChart.Data<>(eightyOneToNinety, "81-90"));
-        series1.getData().add(new XYChart.Data<>(ninetyOneToOneHundred, "91-100"));
-        yAxis.setCategories(FXCollections.observableArrayList("0-10", "11-20", "21-30", "31-40", "41-50", "51-60",
-                "61-70", "71-80", "81-90", "91-100"));
-                */
-        barChart.getData().add(series1);
+        series1.getData().add(new XYChart.Data<>(firstDivision, (int) lowBound + "-" + (int) (lowBound + (1 * divisionSize))));
+        series1.getData().add(new XYChart.Data<>(secondDivision, (int) ((lowBound + (1 * divisionSize)) + 1) + "-" + (int) (lowBound + (2 * divisionSize))));
+        series1.getData().add(new XYChart.Data<>(thirdDivision, (int) ((lowBound + (2 * divisionSize)) + 1) + "-" + (int) (lowBound + (3 * divisionSize))));
+        series1.getData().add(new XYChart.Data<>(fourthDivision, (int) ((lowBound + (3 * divisionSize)) + 1) + "-" + (int) (lowBound + (4 * divisionSize))));
+        series1.getData().add(new XYChart.Data<>(fifthDivision, (int) ((lowBound + (4 * divisionSize)) + 1) + "-" + (int) (lowBound + (5 * divisionSize))));
+        series1.getData().add(new XYChart.Data<>(sixthDivision, (int) ((lowBound + (5 * divisionSize)) + 1) + "-" + (int) (lowBound + (6 * divisionSize))));
+        series1.getData().add(new XYChart.Data<>(seventhDivision, (int) ((lowBound + (6 * divisionSize)) + 1) + "-" + (int) (lowBound + (7 * divisionSize))));
+        series1.getData().add(new XYChart.Data<>(eighthDivision, (int) ((lowBound + (7 * divisionSize)) + 1) + "-" + (int) (lowBound + (8 * divisionSize))));
+        series1.getData().add(new XYChart.Data<>(ninthDivision, (int) ((lowBound + (8 * divisionSize)) + 1) + "-" + (int) (lowBound + (9 * divisionSize))));
+        series1.getData().add(new XYChart.Data<>(tenthDivision, (int) ((lowBound + (9 * divisionSize)) + 1) + "-" + (int) (highBound)));
+        // yAxis Labels must be set manually because of a bug in JavaFx
+        yAxis.setCategories(FXCollections.observableArrayList(
+                (int) lowBound + "-" + (int) (lowBound + (1 * divisionSize)),
+                (int) ((lowBound + (1 * divisionSize)) + 1) + "-" + (int) (lowBound + (2 * divisionSize)),
+                (int) ((lowBound + (2 * divisionSize)) + 1) + "-" + (int) (lowBound + (3 * divisionSize)),
+                (int) ((lowBound + (3 * divisionSize)) + 1) + "-" + (int) (lowBound + (4 * divisionSize)),
+                (int) ((lowBound + (4 * divisionSize)) + 1) + "-" + (int) (lowBound + (5 * divisionSize)),
+                (int) ((lowBound + (5 * divisionSize)) + 1) + "-" + (int) (lowBound + (6 * divisionSize)),
+                (int) ((lowBound + (6 * divisionSize)) + 1) + "-" + (int) (lowBound + (7 * divisionSize)),
+                (int) ((lowBound + (7 * divisionSize)) + 1) + "-" + (int) (lowBound + (8 * divisionSize)),
+                (int) ((lowBound + (8 * divisionSize)) + 1) + "-" + (int) (lowBound + (9 * divisionSize)),
+                (int) ((lowBound + (9 * divisionSize)) + 1) + "-" + (int) (highBound)));
+        barChart.getData().add(series1); 
 
 
     }
 
     //TODO error handling for file not found, etc.
+    // FYI - There won't be a file not found error with the file chooser implementation -JC
     public void onLoadDataClicked() {
 
         FileChooser fileChooser = new FileChooser();
@@ -167,12 +171,14 @@ public class Controller {
                     if (readCSVFile(file)) {
                         display.setText("Data Was Loaded Successfully");
                         display.setStyle("-fx-text-fill: green ;");
+                        logString += "Grade Data Loaded From: " + file + "\n";
                     }
                 } else if (extension.equals("txt")) {
                     grades.clear();
                     if (readTXTFile(file)) {
                         display.setText("Data Was Loaded Successfully");
                         display.setStyle("-fx-text-fill: green ;");
+                        logString += "Grade Data Loaded From: " + file + "\n";
                     }
                 } else {
                     display.setText("File extension: " + extension + " not recognized");
@@ -182,6 +188,12 @@ public class Controller {
         }
     }
 
+    /** 
+     * Reads the grade data from the .csv file.
+     *
+     * @param file - The .csv file to read.c
+     * @return - true if the file was read successfully.
+     */
     private boolean readCSVFile(File file) {
         boolean returnValue = true;
         String line;
@@ -196,12 +208,12 @@ public class Controller {
                 String[] gradeLineStringArray = line.split(cvsSplitBy);
                 if (checkLineCSV(gradeLineStringArray, numberOfLines)) {
                     for (String s : gradeLineStringArray) {
-                    	//TODO parseFloat error handling
+                        //TODO parseFloat error handling
                         Float gradeFloat = Float.parseFloat(s);
                         grades.add(gradeFloat);
                     }
                     updateNumberOfEntries(grades);
-                    populateHistogram(grades);
+                    populateHistogram(grades, highBound, lowBound);
                 } else {
                     returnValue = false;
                 }
@@ -213,6 +225,12 @@ public class Controller {
         return returnValue;
     }
 
+    /**
+     * Reads the grade data from the .txt file.
+     *
+     * @param file - The .txt file to read
+     * @return - true if the file was read successfully.
+     */
     private boolean readTXTFile(File file) {
         boolean returnValue = true;
         String line;
@@ -221,7 +239,7 @@ public class Controller {
 
             while ((line = br.readLine()) != null) {
                 numberOfLines++;
-                if (checkLineTXT(line, numberOfLines)){
+                if (checkLineTXT(line, numberOfLines)) {
                     Float gradeFloat = Float.parseFloat(line.trim());
                     grades.add(gradeFloat);
                 } else {
@@ -233,37 +251,55 @@ public class Controller {
             e.printStackTrace();
             returnValue = false;
         }
-        if (returnValue){
+        if (returnValue) {
             updateNumberOfEntries(grades);
-            populateHistogram(grades);
+            populateHistogram(grades, highBound, lowBound);
         }
         return returnValue;
     }
 
     // TODO Implement checkLine to ensure values in each line of the file are valid
+    // TODO ie that each entry in the array can be parsed to a float - if not, we have the line number to output the required message.
     private boolean checkLineCSV(String[] line, int numberOfLines) {
         return true;
     }
 
     // TODO Implement checkLine to ensure values in each line of the file are valid
+    // TODO ie that there is 1 entry per line and it can be parsed to a float - if not, we have the line number to output the required message.
     private boolean checkLineTXT(String line, int numberOfLines) {
         return true;
     }
 
-    public void updateNumberOfEntries(ArrayList<Float> grades){
+    /**
+     * Updates the number of grade entries in the grades ArrayList.
+     *
+     * @param grades - The ArrayList storing the grades.
+     */
+    public void updateNumberOfEntries(ArrayList<Float> grades) {
         numberOfEntries = grades.size();
         numEntries.setText(String.valueOf(numberOfEntries));
     }
 
+    /**
+     * Sets the upper and lower bounds when the user clicks the "Set Bounds" button.
+     */
     public void onSetBoundsClicked() {
         String highValueText = inputBoundHigh.getText();
         String lowValueText = inputBoundLow.getText();
         if (setUpperBound(highValueText) && setLowerBound(lowValueText)) {
             display.setText("Lower Bound Set To: " + lowValueText + "\n" + "Upper Bound Set to: " + highValueText);
             display.setStyle("-fx-text-fill: green;");
+            logString += "Lower Bound Set To: " + lowValueText + "\n";
+            logString += "Upper Bound Set To: " + highValueText + "\n";
         }
     }
 
+    /**
+     * Tests the upper bound value entered to ensure it can be parsed to a float.
+     *
+     * @param highValueText - The value entered into the inputBoundHigh TextField.
+     * @return - true if the value entered can be converted into a valid float.
+     */
     private boolean setUpperBound(String highValueText) {
         boolean returnValue = true;
         float highValueFloat;
@@ -278,6 +314,12 @@ public class Controller {
         return returnValue;
     }
 
+    /**
+     * Tests the lower bound value entered to ensure it can be parsed to a float.
+     *
+     * @param lowValueText - The value entered into the inputBoundLow TextField.
+     * @return - true if the value entered can be converted into a valid float.
+     */
     private boolean setLowerBound(String lowValueText) {
         boolean returnValue = true;
         float lowValueFloat;
@@ -333,6 +375,9 @@ public class Controller {
 
     }
 
+    /**
+     * Prompts the user to confirm deletion of all grade data.
+     */
     public void onDeleteDataClicked() {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation Dialog");
@@ -340,11 +385,11 @@ public class Controller {
         alert.setContentText("Are you sure you want to delete all grade data?");
 
         Optional<ButtonType> result = alert.showAndWait();
-        if (result.isPresent()){
-            if (result.get() == ButtonType.OK){
+        if (result.isPresent()) {
+            if (result.get() == ButtonType.OK) {
                 // ... user chose OK
                 grades.clear();
-                populateHistogram(grades);
+                populateHistogram(grades, highBound, lowBound);
                 updateNumberOfEntries(grades);
                 display.setText("Grade Data Deleted");
                 display.setStyle("-fx-text-fill: black");
@@ -353,6 +398,7 @@ public class Controller {
 
     }
 
+    // Helper method to print the grade array to the console - can be deleted before final submission.
     private void printGrades(ArrayList<Float> grades) {
         for (Float g : grades) {
             System.out.println(g + " ");
